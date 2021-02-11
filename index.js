54c7588f398718c1071aa5e26b2289c6dffd151f
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-11 15:47:44 -0300

    tester made

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const jsFolder = path.join(__dirname, './public/js');
 
 // Prepare Express
 const app = express();
 
 app.set('view engine', 'nunjucks');
+
+// Read File
+const readFile = function(file) {
+    return '<script>' + fs.readFileSync(path.join(jsFolder, './' + file), 'utf-8') + '</script>';
+};
 
 // Homepage
 const homepageCallback = (req, res) => {
@@ -14,8 +20,18 @@ const homepageCallback = (req, res) => {
     // Webpage
     const webpage = fs.readFileSync(path.join(__dirname, './public/index.html'));
 
+    // Render Page
+    res.send(
+
+        // Web File
+        webpage
+
+        // main.js
+            .replace(`<script src="/js/main.js"></script>`, readFile('main.js'))
+    
+            );
+
     // Complete
-    res.send(webpage);
     return;
 
 };
---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+var pudding = true;
