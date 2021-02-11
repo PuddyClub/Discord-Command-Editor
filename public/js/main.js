10c4c5c4557f60f7d607a861e0337513c972bff9
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-11 20:53:54 -0300

    modal module added

--- public/js/main.js
+++ public/js/main.js
@@ -1,153 +1,160 @@
 $(() => {
 
-    // Template made by dimi130473 (https://bootsnipp.com/snippets/bxzmb)
+    // Exist Storage
+    if (typeof (Storage) !== "undefined") {
 
-    // Div Base
-    const divBase = {};
+        // Template made by dimi130473 (https://bootsnipp.com/snippets/bxzmb)
 
-    // Title
-    divBase.title = $('<h3>', { class: 'title-base text-center text-white pt-5' }).text('Discord Command Editor');
+        // Div Base
+        const divBase = {};
 
-    // Sub Title
-    divBase.subTitle = $('<h3>', { class: 'text-center text-white' }).text('Login');
-
-    // Bot Token
-    divBase.bottoken = $('<div>', { class: 'form-group' }).append(
-
-        // Label
-        $('<label>', { for: 'bottoken', class: 'text-white' }).text('Bot Token:'),
-
-        // Input
-        $('<input>', { type: 'text', name: 'bottoken', id: 'bottoken', class: 'form-control' })
-
-    );
-
-    // Buttons
-    divBase.buttons = {
-        tokenList: $('<input>', { type: 'button', name: 'submit', class: 'btn btn-info btn-md ml-4' }).val('Token List'),
-        oauth2: $('<input>', { type: 'button', name: 'submit', class: 'btn btn-secondary btn-md mr-4' }).val('OAuth2 Login')
-    };
-
-    // Remember
-    divBase.remember = $('<label>', { for: 'remember-me', class: 'text-white' }).append(
-
-        // Text
-        $('<span>', { class: 'mr-2' }).text('Save Token'),
-
-        // Input
-        $('<span>').append(
-            $('<input>', { id: 'remember-me', name: 'remeber-me', type: 'checkbox' })
-        )
-
-    );
-
-    // Form
-    divBase.loginForm = $('<form>', { id: 'login-form', class: 'form' }).append(
+        // Title
+        divBase.title = $('<h3>', { class: 'title-base text-center text-white pt-5' }).text('Discord Command Editor');
 
         // Sub Title
-        divBase.subTitle,
+        divBase.subTitle = $('<h3>', { class: 'text-center text-white' }).text('Login');
 
-        // Username
-        divBase.bottoken,
+        // Bot Token
+        divBase.bottoken = $('<div>', { class: 'form-group' }).append(
 
-        // Options
-        $('<div>', { class: 'form-group' }).append(
-            $('<center>').append(
+            // Label
+            $('<label>', { for: 'bottoken', class: 'text-white' }).text('Bot Token:'),
 
-                // Div
-                $('<div>', {class: 'mb-4'}).append(
+            // Input
+            $('<input>', { type: 'text', name: 'bottoken', id: 'bottoken', class: 'form-control' })
 
-                    // Remember
-                    divBase.remember,
+        );
 
-                    // BR
-                    $('<br>'),
+        // Buttons
+        divBase.buttons = {
+            tokenList: $('<input>', { type: 'button', name: 'submit', class: 'btn btn-info btn-md ml-4' }).val('Token List'),
+            oauth2: $('<input>', { type: 'button', name: 'submit', class: 'btn btn-secondary btn-md mr-4' }).val('OAuth2 Login')
+        };
 
-                    // Buttons
-                    divBase.buttons.oauth2,
-                    $('<input>', { type: 'submit', name: 'submit', class: 'btn btn-primary btn-md' }).val('Login'),
-                    divBase.buttons.tokenList,
+        // Remember
+        divBase.remember = $('<label>', { for: 'remember-me', class: 'text-white' }).append(
 
-                ),
+            // Text
+            $('<span>', { class: 'mr-2' }).text('Save Token'),
 
-                // Privacy Info
-                $('<span>', { class: 'text-white' }).text('All data on this website will be stored in your computer. Your data will only be shared with Discord, Inc.\'s official domain. (https://discord.com)')
-
+            // Input
+            $('<span>').append(
+                $('<input>', { id: 'remember-me', name: 'remeber-me', type: 'checkbox' })
             )
-        ),
 
-    );
+        );
 
-    // Container
-    divBase.container = $('<div>', { class: 'container' }).append(
+        // Form
+        divBase.loginForm = $('<form>', { id: 'login-form', class: 'form' }).append(
 
-        // Row
-        $('<div>', { id: 'login-row', class: 'row justify-content-center align-items-center' }).append(
+            // Sub Title
+            divBase.subTitle,
 
-            // Column
-            $('<div>', { id: 'login-column', class: 'col-md-6' }).append(
+            // Username
+            divBase.bottoken,
 
-                // Box
-                $('<div>', { id: 'login-box', class: 'col-md-12 mb-5' }).append(
+            // Options
+            $('<div>', { class: 'form-group' }).append(
+                $('<center>').append(
 
-                    // Form
-                    divBase.loginForm
+                    // Div
+                    $('<div>', { class: 'mb-4' }).append(
+
+                        // Remember
+                        divBase.remember,
+
+                        // BR
+                        $('<br>'),
+
+                        // Buttons
+                        divBase.buttons.oauth2,
+                        $('<input>', { type: 'submit', name: 'submit', class: 'btn btn-primary btn-md' }).val('Login'),
+                        divBase.buttons.tokenList,
+
+                    ),
+
+                    // Privacy Info
+                    $('<span>', { class: 'text-white' }).text('All data on this website will be stored in your computer. Your data will only be shared with Discord, Inc.\'s official domain. (https://discord.com)')
+
+                )
+            ),
+
+        );
+
+        // Container
+        divBase.container = $('<div>', { class: 'container' }).append(
+
+            // Row
+            $('<div>', { id: 'login-row', class: 'row justify-content-center align-items-center' }).append(
+
+                // Column
+                $('<div>', { id: 'login-column', class: 'col-md-6' }).append(
+
+                    // Box
+                    $('<div>', { id: 'login-box', class: 'col-md-12 mb-5' }).append(
+
+                        // Form
+                        divBase.loginForm
+
+                    )
 
                 )
 
             )
 
-        )
+        );
 
-    );
+        // Root
+        divBase.root = $('<div>', { id: 'login' }).append(
 
-    // Root
-    divBase.root = $('<div>', { id: 'login' }).append(
+            // Title
+            divBase.title.css('display', 'none'),
 
-        // Title
-        divBase.title.css('display', 'none'),
+            // Container
+            divBase.container.css('display', 'none')
 
-        // Container
-        divBase.container.css('display', 'none')
+        );
 
-    );
+        // Start Login Form
+        $('body').append(divBase.root);
+        /* $.LoadingOverlay("hide");
+        $.LoadingOverlay("show", {background: "rgba(0,0,0, 0.5)"}); */
 
-    // Start Login Form
-    $('body').append(divBase.root);
-    /* $.LoadingOverlay("hide");
-    $.LoadingOverlay("show", {background: "rgba(0,0,0, 0.5)"}); */
+        // Get Query
+        const queryURL = {
+            code: Url.queryString('code'),
+            type: Url.queryString('type')
+        };
 
-    // Get Query
-    const queryURL = {
-        code: Url.queryString('code'),
-        type: Url.queryString('type')
-    };
+        // Default Page
+        if (typeof queryURL.code !== "string" || typeof queryURL.type !== "string") {
 
-    // Default Page
-    if (typeof queryURL.code !== "string" || typeof queryURL.type !== "string") {
+            // Login Animation
+            divBase.title.fadeIn(1000);
+            divBase.container.fadeIn(1500);
 
-        // Login Animation
-        divBase.title.fadeIn(1000);
-        divBase.container.fadeIn(1500);
+            // Form Login
+            divBase.loginForm.submit(function () {
 
-        // Form Login
-        divBase.loginForm.submit(function () {
+                // Nothing
+                return false;
 
-            // Nothing
-            return false;
+            });
 
-        });
+        }
 
+        // Login the Code
+        else {
+
+
+
+        }
+
+        // Remove Query
+        Url.removeQuery(false);
+
+    } else {
+        swal("Storage API not found!", "Your browser does not support Storage API!", "error")
     }
-
-    // Login the Code
-    else {
-
-
-
-    }
-
-    // Remove Query
-    Url.removeQuery(false);
 
 });
