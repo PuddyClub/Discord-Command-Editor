81581ceeb266dc543fb7c2281c95561e1110469d
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-11 17:52:45 -0300

    main adapted

--- public/js/main.js
+++ public/js/main.js
@@ -1,82 +1,94 @@
 $(() => {
 
-    // Load Login Base
-    // Template made by dimi130473 (https://bootsnipp.com/snippets/bxzmb)
-    const loginBase = $('<div>', { id: 'login' }).append(
+    // Div Base
+    const divBase = {};
 
-        // Title
-        $('<h3>', { class: 'text-center text-white pt-5' }).text('Discord App Login'),
+    // Title
+    divBase.title = $('<h3>', { class: 'text-center text-white pt-5' }).text('Discord App Login');
 
-        // Container
-        $('<div>', { class: 'container' }).append(
+    // Sub Title
+    divBase.subTitle = $('<h3>', { class: 'text-center text-info' }).text('Login');
 
-            // Row
-            $('<div>', { id: 'login-row', class: 'row justify-content-center align-items-center' }).append(
+    // Username
+    divBase.username = $('<div>', { class: 'form-group' }).append(
 
-                // Column
-                $('<div>', { id: 'login-column', class: 'col-md-6' }).append(
+        // Label
+        $('<label>', { for: 'username', class: 'text-info' }).text('Username:'),
 
-                    // Box
-                    $('<div>', { id: 'login-box', class: 'col-md-12' }).append(
+        // Input
+        $('<input>', { type: 'text', name: 'username', id: 'username', class: 'form-control' })
 
-                        // Form
-                        $('<form>', { id: 'login-form', class: 'form' }).append(
+    );
 
-                            // Sub Title
-                            $('<h3>', { class: 'text-center text-info' }).text('Login'),
+    // Password
+    divBase.password = $('<div>', { class: 'form-group' }).append(
 
-                            // Username
-                            $('<div>', { class: 'form-group' }).append(
+        // Label
+        $('<label>', { for: 'password', class: 'text-info' }).text('Password:'),
 
-                                // Label
-                                $('<label>', { for: 'username', class: 'text-info' }).text('Username:'),
+        // Input
+        $('<input>', { type: 'password', name: 'password', id: 'password', class: 'form-control' })
 
-                                // Input
-                                $('<input>', { type: 'text', name: 'username', id: 'username', class: 'form-control' })
+    );
 
-                            ),
+    // Remember
+    divBase.remember = $('<div>', { class: 'form-group' }).append(
 
-                            // Password
-                            $('<div>', { class: 'form-group' }).append(
+        // Label
+        $('<label>', { for: 'remember-me', class: 'text-info' }).append(
 
-                                // Label
-                                $('<label>', { for: 'password', class: 'text-info' }).text('Password:'),
+            // Text
+            $('<span>', { class: 'mr-2' }).text('Remember me'),
 
-                                // Input
-                                $('<input>', { type: 'password', name: 'password', id: 'password', class: 'form-control' })
+            // Input
+            $('<span>').append(
+                $('<input>', { id: 'remember-me', name: 'remeber-me', type: 'checkbox' })
+            )
 
-                            ),
+        ),
 
-                            // Remember Me
-                            $('<div>', { class: 'form-group' }).append(
+        // BR
+        $('<br>'),
 
-                                // Label
-                                $('<label>', { for: 'remember-me', class: 'text-info' }).append(
+        // Submit
+        $('<input>', { type: 'submit', name: 'submit', class: 'btn btn-info btn-md' }).val('Submit')
 
-                                    // Text
-                                    $('<span>', { class: 'mr-2' }).text('Remember me'),
+    );
 
-                                    // Input
-                                    $('<span>').append(
-                                        $('<input>', { id: 'remember-me', name: 'remeber-me', type: 'checkbox' })
-                                    )
+    // Register
+    divBase.register = $('<div>', { id: 'register-link', class: 'text-right' }).append(
+        $('<a>', { href: '#', class: 'text-info' }).text('Register here')
+    );
 
-                                ),
+    // Container
+    divBase.container = $('<div>', { class: 'container' }).append(
 
-                                // BR
-                                $('<br>'),
+        // Row
+        $('<div>', { id: 'login-row', class: 'row justify-content-center align-items-center' }).append(
 
-                                // Submit
-                                $('<input>', { type: 'submit', name: 'submit', class: 'btn btn-info btn-md' }).val('Submit')
+            // Column
+            $('<div>', { id: 'login-column', class: 'col-md-6' }).append(
 
-                            ),
+                // Box
+                $('<div>', { id: 'login-box', class: 'col-md-12' }).append(
 
-                            // Register
-                            $('<div>', { id: 'register-link', class: 'text-right' }).append(
-                                $('<a>', { href: '#', class: 'text-info' }).text('Register here')
-                            )
+                    // Form
+                    $('<form>', { id: 'login-form', class: 'form' }).append(
 
-                        )
+                        // Sub Title
+                        divBase.subTitle,
+
+                        // Username
+                        divBase.username,
+
+                        // Password
+                        divBase.password,
+
+                        // Remember Me
+                        divBase.remember,
+
+                        // Register
+                        divBase.register
 
                     )
 
@@ -88,8 +100,20 @@ $(() => {
 
     );
 
+    // Load Login Base
+    // Template made by dimi130473 (https://bootsnipp.com/snippets/bxzmb)
+    divBase.root = $('<div>', { id: 'login' }).append(
+
+        // Title
+        divBase.title,
+
+        // Container
+        divBase.container
+
+    );
+
     // Start Login Form
-    $('body').append(loginBase);
+    $('body').append(divBase.root);
     $.LoadingOverlay("hide");
 
 });
