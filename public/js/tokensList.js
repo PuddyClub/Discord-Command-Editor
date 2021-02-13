c200e4f7538bdf984d0488e05bb0ef20b7495d5a
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-13 18:23:02 -0300

    token list update

--- public/js/tokensList.js
+++ public/js/tokensList.js
@@ -3,65 +3,90 @@ dsCommandEditor.tokenList = {
     // Open
     open: function () {
 
+        // Nothing
+        const nothigTokentoShow = function () {
+            eModal.alert({ message: 'No tokens were found!', title: '<i class="fas fa-exclamation-triangle"></i> Error!' });
+        };
+
         // Get Token List
         const tokenList = $.localStorage.get('tokenList');
         if (objType(tokenList, 'object')) {
 
             // Prepare Items
             const tokensList = [];
+            const tokenKeys = Object.keys(tokenList).length;
 
             // Get Items
-            for (const item in tokenList) {
-                tokensList.push($('<tr>').append(
-                    
-                    // ID
-                    $('<td>').text(item),
+            if (tokenKeys) {
+                for (const item in tokenList) {
+                    tokensList.push($('<tr>').append(
 
-                    // Delete
-                    /* Create Delete Button here */
-                    
-                    // Login
-                    $('<td>').append(
-                        $('<button>', { class: 'btn btn-secondary', 'data-dismiss': 'modal' }).text('Login').click(function () {
+                        // ID
+                        $('<td>').text(item),
 
-                            // Start Root
-                            dsCommandEditor.system.startRoot('bot_token', item, tokenList[item].token);
+                        // Actions
+                        $('<td>').append(
 
-                            // Initialize
-                            dsCommandEditor.system.initialize();
+                            // Delete
+                            $('<button>', { class: 'btn btn-danger mr-3' }).text('Delete').click(function () {
+                                eModal.confirm('Are you sure you want to remove this token from your list?', `Delete Item ID "${item}"`)
+                                    .then(function () {
 
-                        })
-                    )
-                ));
+                                        // Remove Item
+                                        $.localStorage.remove('tokenList', item);
+                                        dsCommandEditor.tokenList.open();
+
+                                    });
+                            }),
+
+                            // Login
+                            $('<button>', { class: 'btn btn-secondary', 'data-dismiss': 'modal' }).text('Login').click(function () {
+
+                                // Start Root
+                                dsCommandEditor.system.startRoot('bot_token', item, tokenList[item].token);
+
+                                // Initialize
+                                dsCommandEditor.system.initialize();
+
+                            })
+
+                        )
+
+                    ));
+                }
+
+                eModal.alert({
+                    message: $('<div>', { class: 'table-responsive' }).append(
+                        $('<table>', { class: 'table' }).append(
+
+                            // Head
+                            $('<thead>').append(
+
+                                $('<tr>').append(
+                                    $('<th>').text('ID'),
+                                    $('<th>').text('Actions')
+                                )
+
+                            ),
+
+                            // Body
+                            $('<tbody>').append(tokensList)
+
+                        )
+                    ),
+                    size: 'lg',
+                    title: '<i class="fab fa-discord"></i> Token List'
+                });
+
             }
 
-            eModal.alert({
-                message: $('<div>', { class: 'table-responsive' }).append(
-                    $('<table>', { class: 'table' }).append(
-
-                        // Head
-                        $('<thead>').append(
-
-                            $('<tr>').append(
-                                $('<th>').text('ID'),
-                                $('<th>').text('Token')
-                            )
-
-                        ),
-
-                        // Body
-                        $('<tbody>').append(tokensList)
-
-                    )
-                ),
-                size: 'lg',
-                title: '<i class="fab fa-discord"></i> Token List'
-            })
+            // Nope
+            else { nothigTokentoShow(); }
 
         }
 
         // Empty
-        else { eModal.alert({ message: 'No tokens were found!', title: '<i class="fas fa-exclamation-triangle"></i> Error!' }); }
+        else { nothigTokentoShow(); }
 
     }
 
