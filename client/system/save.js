657a38c55482416ad30c4a7ced2465ab3402ad4e
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-14 17:59:38 -0300

    preparing save file

--- client/system/save.js
+++ client/system/save.js
@@ -2,4 +2,24 @@ dsCommandEditor.system.saveCommandList = function (newCommands, oldCommands) {
 
     console.log(oldCommands, newCommands);
 
+    $.LoadingOverlay("show", { background: "rgba(0,0,0, 0.5)" });
+
+    eModal.alert({
+        message: 'Your command list has been successfully saved!',
+        title: '<i class="fas fa-check"></i> Success!',
+        size: 'lg modal-dialog-centered'
+    });
+
+    $.LoadingOverlay("hide");
+
+    /* 
+    
+    eModal.alert({
+        message: dsCommandEditor.errorModalMessage('Your browser does not support Storage API!'),
+        title: '<i class="fas fa-exclamation-triangle"></i> Storage API not found!',
+        size: 'lg modal-dialog-centered'
+    });
+    
+    */
+
 };
