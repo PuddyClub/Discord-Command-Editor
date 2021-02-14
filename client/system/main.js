3e836c758f4b720e9ee096ae04450b626616c4f8
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-14 18:02:59 -0300

    fetch file

--- client/system/main.js
+++ client/system/main.js
@@ -1,39 +1,5 @@
 dsCommandEditor.system = {
 
-    // Fetch
-    fetch: function (url, method, body) {
-        return new Promise(function (resolve, reject) {
-
-            // Options
-            const fetchOptions = {
-                method: method,
-                headers: { 'Content-Type': 'application/json' },
-                headers: {
-                    'Authorization': `Bot ${dsCommandEditor.root.bot_token}`,
-                    'Content-Type': 'application/json'
-                }
-            };
-
-            // Exist Body
-            if (body) {
-                body.client_id = dsCommandEditor.root.client_id;
-                fetchOptions.body = JSON.stringify(body);
-            }
-
-            // Fetch Function
-            fetch('/' + url, fetchOptions).then(response => {
-                response.json().then(data => {
-                    resolve(data);
-                }).catch(err => {
-                    reject(err);
-                });
-            }).catch(err => {
-                reject(err);
-            });
-
-        });
-    },
-
     // Load Command List
     loadCommandList: function (guildID) {
 
